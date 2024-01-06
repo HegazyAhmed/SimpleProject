376b55ccaa4d9da3f4c8348fe2f0dcfa6c424b78
HegazyAhm <155814263+HegazyAhm@users.noreply.github.com>
2024-01-06 15:14:18 +0200

    Update soso.js

--- links/mostafa/soso.js
+++ links/mostafa/soso.js
@@ -7,3 +7,29 @@ ch[1].addEventListener("click", function () {
   ch[1].classList.add("none");
   console.log(1);
 });
+let buttons = document.querySelectorAll(".mki a button");
+buttons[0].addEventListener("click", function () {
+  window.open("contents/1.pdf");
+});
+buttons[1].addEventListener("click", function () {
+  window.open("contents/2.pdf");
+});
+buttons[2].addEventListener("click", function () {
+  window.open("contents/3.pdf");
+});
+buttons[5].addEventListener("click", function () {
+  window.open("contents/6.pdf");
+});
+buttons[6].addEventListener("click", function () {
+  window.open("contents/7.pdf");
+});
+buttons[7].addEventListener("click", function () {
+  window.open("contents/8.pdf");
+});
+buttons[8].addEventListener("click", function () {
+  window.open("contents/9.pdf");
+});
+buttons[9].addEventListener("click", function () {
+  window.open("contents/10.pdf");
+});
+
