fe4569128985710064d48e5c06788759656b3a13
HegazyAhm <155814263+HegazyAhm@users.noreply.github.com>
2024-01-08 19:06:58 +0200

    Add files via upload

--- links/mostafa/soso.js
+++ links/mostafa/soso.js
@@ -1,12 +1,5 @@
-let ch = document.querySelectorAll(".as");
 let container = document.querySelector(".container");
 
-ch[1].addEventListener("click", function () {
-  container.classList.add("block");
-  ch[0].classList.add("none");
-  ch[1].classList.add("none");
-  console.log(1);
-});
 let buttons = document.querySelectorAll(".mki a button");
 buttons[0].addEventListener("click", function () {
   window.open("contents/1.pdf");
@@ -31,4 +24,26 @@ buttons[8].addEventListener("click", function () {
 });
 buttons[9].addEventListener("click", function () {
   window.open("contents/10.pdf");
+});
+
+const content = document.querySelectorAll(".item");
+content[0].addEventListener("click", function () {
+  CSD[0].classList.toggle("block");
+  CSD[1].classList.toggle("block");
+  CSD[2].classList.toggle("block");
+  CSD[3].classList.toggle("block");
+});
+
+var sidebarBox = document.querySelector("#box");
+var sidebarBtn = document.querySelector("#btn");
+var pageWrapper = document.querySelector("#main-content");
+
+sidebarBtn.addEventListener("click", function (event) {
+  if (this.classList.contains("active")) {
+    this.classList.remove("active");
+    sidebarBox.classList.remove("active");
+  } else {
+    this.classList.add("active");
+    sidebarBox.classList.add("active");
+  }
 });
