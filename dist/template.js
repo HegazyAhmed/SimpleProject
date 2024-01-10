7fead708330ea678d1c0f476b9466609247d7a44
Hegazy <155814263+HegazyAhmed@users.noreply.github.com>
2024-01-10 23:01:59 +0200

    Add files via upload

--- dist/template.js
+++ dist/template.js
@@ -127,3 +127,15 @@ CSDT[0].addEventListener("click", function () {
 });
 /////////////////////////////////////////////////////
 ////slider
+document.getElementById("show-element").onclick = function () {
+  var element = document.getElementById("to-show");
+  if (element.className === "hide") {
+    element.className = "show";
+    document.getElementsByTagName("body")[0].className = "on";
+    document.getElementById("show-element").className = "active";
+  } else {
+    element.className = "hide";
+    document.getElementsByTagName("body")[0].className = "off";
+    document.getElementById("show-element").className = "";
+  }
+}; ///////////////////////////////
